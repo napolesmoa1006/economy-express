4727e22837ec1b5bdf31dba4f49c59163329034e
Carlos Napoles Avila <napolesmoa1006@gmail.com>
2023-10-12 07:29:17 -0400

    Created login view (todo: register)

--- controllers/users.js
+++ controllers/users.js
@@ -3,10 +3,13 @@ const { User } = require('../models')
 
 const index = async (req, res, next) => {
   try {
-    const data = await User.findAll()
-    res.status(200).json({message: 'Success', data})
+    const data = await User.findAll({
+      attributes: { exclude: ['password', 'updatedAt'] }
+    })
+
+    res.status(200).json({ message: 'Success', data })
   } catch (error) {
-    res.status(500).json({message: 'Failed', error})
+    res.status(500).json({ message: 'Failed', error })
   }
 }
 
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
 const express = require('express')
 const router = express.Router()
+// const bcrypt = require('bcrypt')
+// const { User } = require('../models')
+// const { encode } = require('../helpers/jwt-create')
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' })
+})
+
+router.get('/login', (req, res) => {
+  res.render('auth/login', {
+    title: 'Login',
+    erros: []
+  })
+})
+
+router.get('/register', (req, res) => {
+  res.render('auth/register', {
+    title: 'Register',
+    erros: []
+  })
 })
 
 module.exports = router
