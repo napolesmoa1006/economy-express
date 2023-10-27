740a61591bb01a7dc9902bba379d4a585b797945
Carlos Nápoles Avila <napolesmoa1006@gmail.com>
2023-10-27 12:19:03 -0400

    Now the home page is Swagger UI

--- routes/index.js
+++ routes/index.js
@@ -2,22 +2,8 @@ const express = require('express')
 const router = express.Router()
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' })
-})
-
-router.get('/login', (req, res) => {
-  res.render('auth/login', {
-    title: 'Login',
-    erros: []
-  })
-})
-
-router.get('/register', (req, res) => {
-  res.render('auth/register', {
-    title: 'Register',
-    erros: []
-  })
+router.get('/', (req, res) => {
+  res.redirect('/api-docs')
 })
 
 module.exports = router
